cb933b0a353f40221aa2becd78cc16d84e267e17
root-daemon <rahulmarban@gmail.com>
2025-03-22 22:03:03 +0530

    chore: deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { metadata } from "./metadata";
 import ClientLayout from "./client-layout";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export { metadata };
 
@@ -23,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-display`}
+        className={`antialiased font-display`}
       >
         <ClientLayout>{children}</ClientLayout>
       </body>
