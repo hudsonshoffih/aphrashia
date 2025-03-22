0ed355af9d95b13dbe4ab4f5e9c70b9c100144d0
root-daemon <rahulmarban@gmail.com>
2025-03-22 16:55:18 +0530

    style: changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center h-[70vh] px-6 md:px-12 relative z-10">
         <h1 className="text-lg text-secondary font-bold text-center mb-8">#HearThem</h1>
         <div className="relative">
-        <div className="w-[120px] h-[120px] md:w-[160px] md:h-[160px] rounded-full bg-gradient-to-tr from-primary to-secondary shadow-[0_0_200px] md:shadow-[0_0_300px] shadow-primary mb-8" />
-        <div className="rounded-l-full rounded-tr-full rounded-br-lg border-2 border-black absolute md:top-4 top-0 md:-left-26 -left-32 bg-white px-4 py-2">Hi! I am Aphrasis</div>
+          <div className="w-[120px] h-[120px] md:w-[160px] md:h-[160px] rounded-full bg-gradient-to-tr from-primary to-secondary shadow-[0_0_200px] md:shadow-[0_0_300px] shadow-primary mb-8 animate-float-animation" />
+          <div className="rounded-l-full rounded-tr-full rounded-br-lg border-2 border-black absolute md:top-4 top-0 md:-left-26 -left-32 bg-white px-4 py-2 animate-float-delayed">Hi! I am Aphrasis</div>
         </div>
         <h3 className="text-primary font-semibold font-expand">Aphrasis</h3>
         <h2 className="text-xl md:text-4xl text-center mb-6 md:max-w-md max-w-sm font-semibold">Helping through fun and voice-powered learning</h2>
